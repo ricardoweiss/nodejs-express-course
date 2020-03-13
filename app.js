d1682ef6090b3b02fe55374b8af619d5a9554ad5
Ricardo Weiss <ricardohsweiss@gmail.com>
2020-03-13 10:15:36 -0300

    initial commit, simple express js code

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/add-product',(req, res, next) => {
+    res.send('<form action="/product" method="post"><input type="text" name="title"><button type="submit">Send</button></form>');
+});
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>Hello from express</h1>');
+});
+
+
+app.listen(3000);
