fd2ebfb4059d39e7e5cec4e6ee885e2147e7bc3a
Ricardo Weiss <ricardohsweiss@gmail.com>
2020-03-13 11:00:15 -0300

    created the routes folder, and moved the routes to each file

--- app.js
+++ app.js
@@ -1,22 +1,15 @@
 const express = require('express');
+
 const bodyParser = require('body-parser');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use('/add-product',(req, res, next) => {
-    res.send('<form action="/product" method="post"><input type="text" name="title"><button type="submit">Send</button></form>');
-});
-
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-    res.send('<h1>Hello from express</h1>');
-});
-
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
