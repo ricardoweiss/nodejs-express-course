8fed812f1f8cdee411f2bd80e2f915d7d2a2c458
Ricardo Weiss <ricardohsweiss@gmail.com>
2020-03-14 10:46:39 -0300

    filtering paths

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="post"><input type="text" name="title"><button type="submit">Send</button></form>');
+    res.send('<form action="/admin/product" method="post"><input type="text" name="title"><button type="submit">Send</button></form>');
 });
 
 router.post('/product', (req, res, next) => {
