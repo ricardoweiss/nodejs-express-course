e682a395c08b039872248110fdc1c1e9a6e213be
Ricardo Weiss <ricardohsweiss@gmail.com>
2020-03-14 11:24:33 -0300

    serving html files instead of dummy html's

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,14 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-    res.send('<form action="/admin/product" method="post"><input type="text" name="title"><button type="submit">Send</button></form>');
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
