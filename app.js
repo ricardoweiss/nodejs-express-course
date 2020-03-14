7d4c94bc5625755f07d46031e8d18212b554ce9e
Ricardo Weiss <ricardohsweiss@gmail.com>
2020-03-14 10:40:00 -0300

    404 handling

--- app.js
+++ app.js
@@ -12,4 +12,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).send('<h1>404 Page not found</h1>')
+});
+
 app.listen(3000);
